6e66400bc3fbca2e5f4b3a0121646e18ac8c2522
Filipe Daniel Wille <filipew2000@hotmail.com>
2022-05-10 15:23:18 -0300

    continuing classes, added TS to project

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,11 +1,11 @@
 export const RepositoryItem = (props)=> {
     return (
         <li>
-        <strong>{props.repository?.name ?? 'Default'}</strong>
-        <p>{props.repository?.description}</p>
+        <strong>{props.repository.name}</strong>
+        <p>{props.repository.description}</p>
 
 
-        <a href={props.repository?.link}>
+        <a href={props.repository?.html_url}>
             Acessar repositório
         </a>
     </li>
--- src/index.jsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from 'react-dom/client'; //Usa esse modelo agora
-// import {render} from 'react-dom'; Usado na versão do React 17
-import { App } from './App';
-
-// render(<App />, document.getElementById('root')) Usado na versão do React 17
-
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<App />);
