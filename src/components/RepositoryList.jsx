9c535285956242305371b070e27d35556b909b0f
Filipe Daniel Wille <filipew2000@hotmail.com>
2022-05-10 13:15:06 -0300

    first commit

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,26 @@
+import { RepositoryItem } from "./RepositoryItem";
+
+import '../styles/repositories.scss';
+
+const repository = {
+    name: 'unform22',
+    description: 'Forms in React',
+    link: 'https://google.com.br'
+}
+
+export function RepositoryList() {
+    return (
+        <section className="repository-list">
+            <h1>Lista de repositórios</h1>
+
+            <ul>
+               <RepositoryItem repository={repository} />
+               <RepositoryItem repository={repository} />
+               <RepositoryItem repository={repository} />
+               <RepositoryItem repository={repository} />
+
+            </ul>
+
+        </section>
+    )
+}
